2d57c63cc651e4acc10f6e5f0a72ac31ee8847a5
IndreshSen <indreshsen100@gmail.com>
2025-11-21 23:15:59 +0530

    create all file with stru

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
+//https://youtu.be/lpx2zFkapIk?si=EZZ-pkHyDhv3JrB1
