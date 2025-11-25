edd9473553029e3cb7b85f3b633bb09806c99f0a
IndreshSen <indreshsen100@gmail.com>
2025-11-25 16:17:03 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
-
+import Login from './pages/Login/Login'
 import { Routes,Route } from 'react-router-dom'
 const App = () => {
+ // localStorage(clear)
   return (
     <div>
       <Routes>
            <Route path ='/' element={<Home/>}/>
+          <Route path ='/login' element={<Login/>}/>
       </Routes>
-      <Home/>
     </div>
+    
+  
   )
 }
 
