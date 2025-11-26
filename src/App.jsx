d29939050693eea7d4d5bda70813181277612fef
IndreshSen <indreshsen100@gmail.com>
2025-11-26 18:23:21 +0530

    player

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
 import { Routes,Route } from 'react-router-dom'
 const App = () => {
  // localStorage(clear)
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
            <Route path ='/' element={<Home/> }/>
           <Route path ='/login' element={<Login/>}/>
+           <Route path ='/player/:id' element={<Player/>}/>
       </Routes>
     </div>
     
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 const Player = () => {
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" />
+      <iframe width="90%" height="90%" 
+      src="https://www.youtube.com/embed/J7raJU_8GXU?si=e4Y38BsgLIwvF6Dt&amp;start=1" title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>Published Data</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
   )
 }
 
