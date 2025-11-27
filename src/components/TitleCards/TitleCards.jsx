e5c1194fefa8c22a20210449c08117c57bf29a1c
IndreshSen <indreshsen100@gmail.com>
2025-11-27 17:08:54 +0530

    player api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -47,40 +47,8 @@
 //   );
 // };
 
-// export default TitleCards;
-
-// TitleCards.jsx
-// import React, { useRef } from "react";
-// import "./TitleCards.css";
-// import cards_data from "../../assets/cards/Cards_data";
-
-// const TitleCards = () => {
-//   const cardsRef = useRef(null);
-
-//   const handleWheel = (e) => {
-//     if (!cardsRef.current) return;
-//     cardsRef.current.scrollLeft += e.deltaY;
-//   };
-
-//   return (
-//     <div className="title-cards">
-//       <h2>Popular on Netflex</h2>
-//       <div className="card-list" ref={cardsRef} onWheel={handleWheel}>
-//         {cards_data.map((card, index) => (
-//           <div className="card" key={index}>
-//             <img src={card.image} alt={card.name} />
-//             <p>{card.name}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TitleCards;
 
 
-// TitleCards.jsx
 import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
 const Player = () => {
+
+ const [apiData,setApiData] =useState({
+  name:'',
+  key : '',
+  published_at:'',
+  typeof:''
+ })
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZmEzNzcyNWE1NjgwZjlmY2VhMDEwMmJjMjUyNDRlMiIsIm5iZiI6MTc2NDA3NzY4NS41NzcsInN1YiI6IjY5MjViMDc1ZmRlZDY4YzQwZTMwYzhlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._K25PQBKU0yeo9ZmRW4VHJanLejo1-zWX_Wm_qPENB0'
+  }
+};
+
+
+useEffect(()=>{
+fetch('https://api.themoviedb.org/3/tv/603692/videos?language=en-US', options)
+  .then(response => response.json())
+  .then(response => setApiData(response.results[0]))
+  .catch(err => console.error(err));
+},[])
+
+
+  
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" />
       <iframe width="90%" height="90%" 
-      src="https://www.youtube.com/embed/J7raJU_8GXU?si=e4Y38BsgLIwvF6Dt&amp;start=1" title='trailer' frameBorder='0' allowFullScreen></iframe>
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>Published Data</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )
 }
 
 export default Player
+
+
+
+
+
+
